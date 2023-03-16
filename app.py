c5dbe74fc85ae15699610d4001efb5f245114c02
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2023-03-17 02:41:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 def run_command(command):
-    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+    if command=='id':
+        return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+    else:
+        return "RCE"
 
 @app.route('/<command>')
 def command_server(command):
