0f4ff2c44554bb9bcdd0b03ae8f8a6210462ccfa
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2023-03-17 02:39:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import subprocess
+
+from flask import Flask
+app = Flask(__name__)
+
+def run_command(command):
+    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
+
+@app.route('/<command>')
+def command_server(command):
+    return run_command(command)
