7507464811f48e95652c7ccaffde09cfd00bac6c
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2023-03-19 17:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def run_command(command):
     #command = shlex.quote(command)
     #ot = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE).communicate()[0]
-    ot = subprocess.Popen(['cat', command])
+    ot = subprocess.Popen(['cat', '/python-docker/app.py', command])
     return ot
 
 @app.route('/<command>')
